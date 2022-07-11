fb929ae44b10ee579714c1a3c7a4e8bb018d3533
JumpyLionnn <fwipsity@gmail.com>
2022-07-11 13:11:41 +0300

    add a stay on top feature

--- index.ts
+++ index.ts
@@ -1,34 +1,48 @@
-import {app, BrowserWindow} from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
 import * as path from 'path';
+import * as fs from "fs";
 
 function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    title: "RetroMMO",
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-    
-  });
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: "RetroMMO",
+        webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+        }
+        
+    });
 
-  win.setMenuBarVisibility(false);
+    win.webContents.openDevTools();
 
-  win.loadFile('index.html');
+    win.setMenuBarVisibility(false);
+
+    win.maximize();
+    win.loadURL("https://retrommo2.herokuapp.com");
+    win.webContents.on('did-finish-load', () => {
+        fs.readFile("style.css", {encoding: "utf-8"}, (error, css) => {
+            win.webContents.insertCSS(css);
+        });
+    });
+
+    ipcMain.addListener("stayOnTopStateChange", (event, state: {value: boolean}) => {
+        win.setAlwaysOnTop(state.value);
+    });
+  
 }
 
 app.whenReady().then(() => {
-  createWindow();
+    createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  });
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    });
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
--- preload.ts
+++ preload.ts
@@ -1,1 +1,40 @@
-import { contextBridge } from "electron";
+import { ipcRenderer } from 'electron';
+import { off } from 'process';
+
+function createElement<T extends HTMLElement>(tag: string, options: {[name: string]: any} = {}, parent?: HTMLElement): T{
+    const element = document.createElement(tag);
+    if(options.class)
+    {
+        if(typeof options.class == "string"){
+            element.classList.value = options.class;
+        }
+        else if(Array.isArray(options.class)){
+            element.classList.value = options.class.join(" ");
+        }
+    }
+    Object.assign(element, options);
+    delete (<any>element).class;
+    if(parent){
+        parent.appendChild(element);
+    }
+    return <T>element;
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const actionsPanel = document.querySelector<HTMLDivElement>("#game\\/sidebar\\/content\\/actions\\/box")!;
+
+    const stayOnTopInput = createElement<HTMLInputElement>("input", {type: "checkbox", id: "desktop/game/sidebar/content/settings/top", class: "desktop"});
+    
+    createElement("label", {for: "desktop/game/sidebar/content/settings/top", innerText: "Stay On Top"}, actionsPanel);
+    actionsPanel.appendChild(stayOnTopInput);
+    
+
+    const fpsCounter = document.querySelector<HTMLParagraphElement>("#game\\/main\\/screen\\/fps-count")!;
+
+    stayOnTopInput.addEventListener("input", () => {
+        ipcRenderer.send("stayOnTopStateChange", {value: stayOnTopInput.checked});
+        fpsCounter.hidden = stayOnTopInput.checked;
+    });
+});
+  
