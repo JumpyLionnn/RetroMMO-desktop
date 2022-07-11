080f1e35ac6ed133703e37d7a2d4311f66827356
JumpyLionnn <fwipsity@gmail.com>
2022-07-11 11:16:03 +0300

    added a basic RetroMMO client

---
+++ index.ts
@@ -0,0 +1,31 @@
+import {app, BrowserWindow} from 'electron';
+import * as path from 'path';
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: "RetroMMO",
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
